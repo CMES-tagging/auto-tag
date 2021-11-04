352aab5c893fd142d3ebe42f88cca8b7849eaee2
Stephen Davison <sdavison@caltech.edu>
2021-11-03 23:10:21 -0700

    Removed extra files

--- auto-tag/extract_text.py
+++
@@ -1,8 +0,0 @@
-# convert PDF; split into pages and lines
-import pdftotext
-
-def pdf_to_text(filename):
-    with open(filename, 'rb') as f:
-        doc = [page.split('\n') for page in pdftotext.PDF(f)]
-    return doc
-
--- auto-tag/extras.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# clear screen for Windows, Mac, Linux OS
-def clear():
-    if os.name == 'nt':
-        _ = os.system('cls')
-    else:
-        _ = os.system('clear')
